2b598f19d75864505945145ad14b8c2d573ef9cc
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-11-29 09:47:10 +0100

    correcttion Readme

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // PROBLEMA: calcolare il prezzo totale del viaggio in treno; facendo attenzione all'età del passeggero, km percorsi e alle relative scontistiche applicate in base all'età e emettere il biglietto.
 
-//1-Permettere all'utente di inserire il nome nell'input
 
-//2-Permettere all'utente di inserire il numero di km che vuole percorrere nell'input
+//1-Permettere all'utente di inserire il numero di km che vuole percorrere nell'input
+    //1.1-Realizzare input per km
 
-//3-Permettere all'utente di selezionare se minorenne,maggiorenne o over nell'input
 
-//4-calcolare il prezzo
-//  4.1-SE < di 18 calcolare lo sconto
-//  4.2-SE > di 65 calcolare lo sconto
-//  4.3-ALTRIMENTI calcolare il prezzo pieno
+//2-Permettere all'utente di selezionare se minorenne,maggiorenne o over nell'input
+    //2.1Realizzare input per età
 
-//5-Visualizzare in pagina il biglietto con nome e prezzo biglietto scontato o meno
+// 3-Permettere all'utente di inserire Nome e cognome utente
+    //3.1Realizzare input per età
+
+//4-calcolare il prezzo (0.21 * KM percorsi) alla pressione di un bottone
+
+    //4.1-SE < di 18 calcolare lo sconto
+
+    //4.2-SE > di 65 calcolare lo sconto
+
+    //4.3-ALTRIMENTI calcolare il prezzo pieno
+
+
+//5-Mostrare a video il biglietto con nome e prezzo biglietto scontato o meno
+
+//6-Realizzare html per visualizzare risultati
